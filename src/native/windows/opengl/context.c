ee04bc676ed4e84cef28cf570d39441414fcd5a2
spasi <spasi@878f3ca7-6210-0410-8345-df2f93afa42f>
2012-12-19 12:38:50 +0000

    Fixed commit error.
    git-svn-id: https://java-game-lib.svn.sourceforge.net/svnroot/java-game-lib/trunk/LWJGL@3837 878f3ca7-6210-0410-8345-df2f93afa42f

--- src/native/windows/opengl/context.c
+++ src/native/windows/opengl/context.c
@@ -224,7 +224,6 @@ static int findPixelFormatARBFromBPP(JNIEnv *env, HDC hdc, WGLExtensions *extens
 		putAttrib(&attrib_list, WGL_ACCELERATION_ARB); putAttrib(&attrib_list, WGL_FULL_ACCELERATION_ARB);
 	}
 	putAttrib(&attrib_list, WGL_PIXEL_TYPE_ARB); putAttrib(&attrib_list, pixel_type);
-	putAttrib(&attrib_list, WGL_DOUBLE_BUFFER_ARB); putAttrib(&attrib_list, double_buffer ? TRUE : FALSE);
 	if ( double_buffer || samples == 0 ) {
 		// Skip this for FALSE + MSAA: NV drivers won't return any PixelFormat
 		putAttrib(&attrib_list, WGL_DOUBLE_BUFFER_ARB); putAttrib(&attrib_list, double_buffer ? TRUE : FALSE);
